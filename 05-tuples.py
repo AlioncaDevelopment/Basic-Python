ebbc84455dc408450bac32111efd145038e3a425
Bárbara Dinorah <bdinorah@REACCIONA.LOCAL>
2022-12-19 11:25:13 +0100

    clase 5 tuplas

--- 05-tuples.py
+++ 05-tuples.py
@@ -5,3 +5,32 @@
 
 #Declaracion de variables
 my_tupla = tuple();
+my_tupla2 = ();
+
+#añadimos datos
+my_tupla = (36, 1.70, 'Alionca', 'Ignacio')
+my_tupla2 = (36, 60, 30)
+
+#pintamos contenido
+print(my_tupla2)
+print(my_tupla)
+
+#mostrar el tipo de variable
+print(type(my_tupla))
+
+# Acceso a elementos y búsqueda
+print(my_tuple[0])
+print(my_tuple[-1])
+# print(my_tuple[4]) IndexError
+# print(my_tuple[-6]) IndexError
+
+print(my_tuple.count("Brais"))
+print(my_tuple.index("Moure"))
+print(my_tuple.index("Brais"))
+
+
+
+
+
+
+
